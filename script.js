1df8af8e899eb57e5c9bf56a36fcca85f477a59f
Iamneo <techadmin@iamneo.ai>
2023-09-02 02:33:21 +0000

    Backup Commit

--- script.js
+++ script.js
@@ -1,39 +1,31 @@
-// JavaScript variables to track game state
+
 let currentPlayer = 'X';
 let gameBoard = ['', '', '', '', '', '', '', '', ''];
 let gameActive = true;
 
-// Select all input fields with class "btn"
 const cells = document.querySelectorAll('.btn');
 
-// Function to handle cell click event
 function handleCellClick(e) {
     const cellIndex = parseInt(e.target.dataset.index);
 
-    // Check if the cell is empty and the game is active
     if (gameBoard[cellIndex] === '' && gameActive) {
-        // Update the game board and display the current player's move
         gameBoard[cellIndex] = currentPlayer;
         e.target.value = currentPlayer;
 
-        // Check for a win or a draw
         if (checkWin() || checkDraw()) {
             endGame();
         } else {
-            // Switch to the next player
             currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
             updateResult();
         }
     }
 }
 
-// Function to check for a win
 function checkWin() {
-    // Define winning combinations
     const winCombos = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6]             // Diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ];
 
     for (const combo of winCombos) {
@@ -45,28 +37,23 @@ function checkWin() {
     return false;
 }
 
-// Function to check for a draw
 function checkDraw() {
     return !gameBoard.includes('');
 }
 
-// Function to end the game
 function endGame() {
     gameActive = false;
     document.querySelector('.reset-btn').disabled = false;
 }
 
-// Function to update the result container
 function updateResult() {
     document.querySelector('.result-container').textContent = `Player ${currentPlayer}'s turn`;
 }
 
-// Add event listeners to each cell for clicks
 cells.forEach(cell => {
     cell.addEventListener('click', handleCellClick);
 });
 
-// Function to reset the game
 function resetGame() {
     currentPlayer = 'X';
     gameBoard = ['', '', '', '', '', '', '', '', ''];
@@ -80,8 +67,6 @@ function resetGame() {
     document.querySelector('.result-container').textContent = `Player ${currentPlayer}'s turn`;
 }
 
-// Add event listener to the reset button
 document.querySelector('.reset-btn').addEventListener('click', resetGame);
 
-// Initialize the game
 updateResult();
